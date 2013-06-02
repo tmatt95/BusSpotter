9ed9b5e77025062fdfce3207a89f1c2e500fcbfc
Matt Turner <tmatt95@gmail.com>
2013-06-02 16:03:45 +0100

    Starting work on the about page

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -4,8 +4,15 @@ $this->pageTitle=Yii::app()->name . ' - About';
 <h1>About Us</h1>
 
 <h2>The Problem</h2>
+<ul>
+    <li><h5>A lot of information is never written down</h5>
+    <p>There are lots of very interesting bus stories through word of mouth which never get written down.</p>
+    </li>
+    <li>No easy to access repository for bus specific information</li>
+</ul>
 
 <h2>Our Goal</h2>
-
-<p>This is a "static" page. You may change the content of this page
-by updating the file <code><?php echo __FILE__; ?></code>.</p>
+<ul>
+    <li>To create a fun way of recording bus spottings</li>
+    <li>To document the interesting history of buses throughout the world</li>
+</ul>
