b624787ab16c95503f658b852ced3f14cc3dac9e
Matt Turner <tmatt95@gmail.com>
2013-06-07 22:58:18 +0100

    revising about page content

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -1,18 +1,32 @@
 <?php
-$this->pageTitle=Yii::app()->name . ' - About';
+$this->pageTitle = Yii::app()->name . ' - About';
 ?>
 <h1>About Us</h1>
-
-<h2>The Problem</h2>
-<ul>
-    <li><h5>A lot of information is never written down</h5>
-    <p>There are lots of very interesting bus stories through word of mouth which never get written down.</p>
-    </li>
-    <li>No easy to access repository for bus specific information</li>
-</ul>
-
-<h2>Our Goal</h2>
-<ul>
-    <li>To create a fun way of recording bus spottings</li>
-    <li>To document the interesting history of buses throughout the world</li>
-</ul>
+<div class="row">
+    <div class="span6">
+        <h2>The Problems</h2>
+        <ul>
+            <li><h5>A lot of information is never written down</h5>
+                <p>There are lots of very interesting bus stories spread through word of mouth which never get written down. It would be a shame to loose these. Through commenting on the vechiles we will be able to create an archive of these and hopefully there never loose them. <a href="#">Find out about commenting on Bus Spotter...</a></p>
+            </li>
+            <li>
+                <h5>No easy to access repository for bus specific information</h5>
+            </li>
+        </ul>
+    </div>
+    <div class="span6">
+        <h2>Our Goal</h2>
+        <ul>
+            <li>
+                <h5>A place where people can record their bus stories</h5>
+            </li>
+            <li>
+                <h5>A fun way of recording bus spottings</h5>
+                <p>To create a system which allows people to record information on the buses they see when they see them. </p>
+            </li>
+            <li>
+                <h5>To document the vehicles interesting histories</h5>
+            </li>
+        </ul>
+    </div>
+</div>
