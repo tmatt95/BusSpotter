4e67057596d6ee87580031d4da8740bed035abc8
Matt Turner <tmatt95@gmail.com>
2013-06-07 22:58:05 +0100

    Moving media of month to partial

--- protected/views/site/partials/_mediaOfTheMonth.php
+++ protected/views/site/partials/_mediaOfTheMonth.php
@@ -2,7 +2,7 @@
     <div class="row">
 	<div class="span10">
 	    <h2>Media of the Month</h2>
-	    <p>Every month we will hold a competition and place the winning Youtube video in the space below!</p>
+	    <p class="muted">Every month we will hold a competition and place the winning Youtube video in the space below!</p>
 	</div>
 	<div class="span2 rightMenu"><a href="#" class="rightLink" data-bind="click:openDisclaimer">Disclaimer</a></div>
     </div>
