617bbcc9595230261df539a32ea1cd70ca08d00c
Matt Turner <tmatt95@gmail.com>
2013-06-07 22:58:45 +0100

    Tidying up the error screen

--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -1,15 +1,12 @@
 <?php
 /* @var $this SiteController */
 /* @var $error array */
-
 $this->pageTitle=Yii::app()->name . ' - Error';
-$this->breadcrumbs=array(
-	'Error',
-);
 ?>
-
+<div class="alert alert-error" style="width: 550px; margin: 0px auto;">
 <h2>Error <?php echo $code; ?></h2>
-
-<div class="error">
 <?php echo CHtml::encode($message); ?>
+<hr />
+<p>The problem has been logged and will be investigated ASAP. If you would like
+to be contacted about the problem. Please fill in the <a href="#">contact form</a>... we wont bite!</p>
 </div>
