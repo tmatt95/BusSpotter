d6204740b64ecffa7e00480cc73b22774afd0984
Matt Turner <tmatt95@gmail.com>
2013-06-07 19:43:10 +0100

    Starting to add the register form
    Starting to add the register form so I can log into the system again!

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -2,52 +2,56 @@
 /* @var $this SiteController */
 /* @var $model LoginForm */
 /* @var $form CActiveForm  */
-
-$this->pageTitle=Yii::app()->name . ' - Login';
-$this->breadcrumbs=array(
-	'Login',
-);
+$this->pageTitle = Yii::app()->name . ' - Login';
 ?>
-
-<h1>Login</h1>
-
-<p>Please fill out the following form with your login credentials:</p>
-
-<div class="form">
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'login-form',
-	'enableClientValidation'=>true,
-	'clientOptions'=>array(
-		'validateOnSubmit'=>true,
-	),
-)); ?>
-
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'username'); ?>
-		<?php echo $form->textField($model,'username'); ?>
-		<?php echo $form->error($model,'username'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'password'); ?>
-		<?php echo $form->passwordField($model,'password'); ?>
-		<?php echo $form->error($model,'password'); ?>
-		<p class="hint">
-			Hint: You may login with <kbd>demo</kbd>/<kbd>demo</kbd> or <kbd>admin</kbd>/<kbd>admin</kbd>.
-		</p>
-	</div>
-
-	<div class="row rememberMe">
-		<?php echo $form->checkBox($model,'rememberMe'); ?>
-		<?php echo $form->label($model,'rememberMe'); ?>
-		<?php echo $form->error($model,'rememberMe'); ?>
-	</div>
-
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Login'); ?>
-	</div>
-
-<?php $this->endWidget(); ?>
-</div><!-- form -->
+<div class="row">
+    <div class="span6">
+        <?php
+        $form = $this->beginWidget('CActiveForm', array(
+            'id' => 'register-form',
+            'enableClientValidation' => true,
+            'clientOptions' => array(
+                'validateOnSubmit' => true,
+            ),
+        ));
+        ?>
+        <legend>Register</legend>
+        <?php $this->endWidget(); ?>
+    </div>
+    <div class="span6">
+        <?php
+        $form = $this->beginWidget('CActiveForm', array(
+            'id' => 'login-form',
+            'enableClientValidation' => true,
+            'clientOptions' => array(
+                'validateOnSubmit' => true,
+            ),
+            'htmlOptions' => array('class' => 'form-horizontal')
+        ));
+        ?>
+        <legend>Login</legend>
+        <div class="control-group">
+            <?php echo $form->labelEx($model, 'username', array('class' => 'control-label')); ?>
+            <div class="controls">
+                <?php echo $form->textField($model, 'username'); ?>
+                <?php echo $form->error($model, 'username'); ?>
+            </div>
+        </div>
+        <div class="control-group">
+            <?php echo $form->labelEx($model, 'password', array('class' => 'control-label')); ?>
+            <div class="controls">
+                <?php echo $form->passwordField($model, 'password'); ?>
+                <?php echo $form->error($model, 'password'); ?>
+            </div>
+        </div>
+        <div class="control-group">
+            <div class="controls">
+                <?php echo $form->checkBox($model, 'rememberMe'); ?>
+                <?php echo $form->label($model, 'rememberMe'); ?>
+                <?php echo $form->error($model, 'rememberMe'); ?>
+                <?php echo CHtml::submitButton('Login', array('class' => 'btn btn-primary')); ?>
+            </div>
+        </div>
+        <?php $this->endWidget(); ?>
+    </div>
+</div>
