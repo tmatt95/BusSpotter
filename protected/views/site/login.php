a19b29a11e8842af86a0b3fc53a24b1620760dde
Matt Turner <tmatt95@gmail.com>
2013-06-08 10:39:55 +0100

    Starting to add javascript to the register function

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -16,12 +16,16 @@ $this->pageTitle = Yii::app()->name . ' - Login';
         ));
         ?>
         <legend>Register</legend>
-        <?php $this->endWidget(); ?>
-        <p>Register form</p>
-        <p>Add captcha here!</p>
-        <div class="alert alert-success">
-            <h2>Welcome Aboard!</h2>
-            <p>Please check your e-mail for a link to activate your account</p>
+        <div data-bind="if:registerSuccessful() == 'False'">
+            <?php $this->endWidget(); ?>
+            <p>Register form</p>
+            <p>Add captcha here!</p>
+        </div>
+        <div data-bind="if:registerSuccessful() == 'True'">
+            <div class="alert alert-success">
+                <h2>Welcome Aboard!</h2>
+                <p>Please check your e-mail for a link to activate your account</p>
+            </div>
         </div>
     </div>
     <div class="span6">
@@ -29,7 +33,7 @@ $this->pageTitle = Yii::app()->name . ' - Login';
         $form = $this->beginWidget('CActiveForm', array(
             'id' => 'login-form',
             'enableClientValidation' => true,
-             'errorMessageCssClass'=>'text-error',
+            'errorMessageCssClass' => 'text-error',
             'clientOptions' => array(
                 'validateOnSubmit' => true,
             ),
@@ -54,7 +58,7 @@ $this->pageTitle = Yii::app()->name . ' - Login';
         <div class="control-group">
             <div class="controls">
                 <?php echo $form->checkBox($model, 'rememberMe'); ?>
-                <?php echo $form->label($model, 'rememberMe',array('style'=>'display: inline;position: relative; top: 3px;')); ?>
+                <?php echo $form->label($model, 'rememberMe', array('style' => 'display: inline;position: relative; top: 3px;')); ?>
                 <?php echo $form->error($model, 'rememberMe'); ?>
             </div>
         </div>
@@ -66,3 +70,10 @@ $this->pageTitle = Yii::app()->name . ' - Login';
         <?php $this->endWidget(); ?>
     </div>
 </div>
+<script type="text/javascript">
+    function ViewModel() {
+        // Register
+        self.registerSuccessful = ko.observable('False');
+    }
+    ko.applyBindings(new ViewModel());
+</script>
